531de1994bd74c611cf4553334a0993225072eaa
tjrwls234 <tjrwls234@gmail.com>
2023-01-06 04:29:54 +0900

    first commit

---
+++ src/main/java/com/study/study_springboots/controller/BoardController.java
@@ -0,0 +1,34 @@
+package com.study.study_springboots.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BoardController {
+    @RequestMapping(value = { "/board", "/board/list" }, method = RequestMethod.GET)
+    public String list() {
+        return "/WEB-INF/views/board/list.jsp";
+    }
+
+    @RequestMapping(value = "/board/view", method = RequestMethod.GET)
+    public String view() {
+        return "/WEB-INF/views/board/view.jsp";
+    }
+
+    @RequestMapping(value = "/board/edit", method = RequestMethod.GET)
+    public String edit() {
+        return "/WEB-INF/views/board/edit.jsp";
+    }
+
+    @RequestMapping(value = "/board/form", method = RequestMethod.GET)
+    public String form() {
+        return "/WEB-INF/views/board/form.jsp";
+    }
+
+    @RequestMapping(value = "/board/form", method = RequestMethod.POST)
+    public String formPost() {
+        // insert biz
+        return "/WEB-INF/views/board/list.jsp";
+    }
+}
